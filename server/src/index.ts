f5cc80b7a2e3ec7e4dc5cbb866fe5f04b6257484
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2026-05-31 16:33:13 +0900

    chat機能を追加した

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { HonoRequest } from 'hono';
+import { env } from 'hono/adapter';
+import Groq from 'groq-sdk';
 
 const app = new Hono();
 
@@ -13,8 +16,32 @@ app.get('/', (c) => {
   });
 });
 
-app.get('/health', (c) => {
-  return c.json({ status: 'healthy' });
+app.post('/groq/chat', async (c) => {
+  const bodyJson = await parseJsonBody(c.req);
+  const { GROQ_API_KEY } = env(c);
+  const groq = new Groq({ apiKey: GROQ_API_KEY as string });
+  const chatCompletion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content: bodyJson.message,
+      },
+    ],
+    model: 'llama-3.3-70b-versatile',
+  });
+  return c.json(chatCompletion);
 });
+
+async function parseJsonBody(req: HonoRequest): Promise<any> {
+  const bodyText = await req.text();
+  if (bodyText) {
+    try {
+      return JSON.parse(bodyText);
+    } catch (e) {
+      return {};
+    }
+  }
+  return {};
+}
 
 export default app;
